a891b1d0efb535c9f55ade617126c6e19dc3263b
unknown <JR2401@bijisen.local>
2025-12-11 12:48:12 +0900

    欠席届の提出

--- app/controllers/absences_controller.rb
+++ app/controllers/absences_controller.rb
@@ -1,2 +1,23 @@
 class AbsencesController < ApplicationController
+
+
+  def index
+    @absence = Absence.new
+  end
+
+  def create
+    @absence = current_student.absences.build(absence_params)
+    
+    if @absence.save
+      redirect_to absences_path, notice: "欠席届を提出しました。"
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def absence_params
+    params.require(:absence).permit(:kind, :reason, :detail, :target_date, periods: [])
+  end
 end
